29d2e65bb6bced7a785e808740c6caf0a8bc40a6
rakun256 <emreuslu256@gmail.com>
2024-02-23 23:41:46 +0300

    Sitenin genel yapısı hazırlandı, hazır olan Navbar componenti eklendi ve hafif düzenlemeler yapıldı.

---
+++ src/Components/ImageFrame/imageFrame.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./imageFrame.css";
+
+const ImageFrame = () => {
+  return (
+    <div className="Image_Frame">
+      <img src="/images/ytu_dp2.jpg" alt="Matmuh_image" />
+    </div>
+  );
+};
+
+export default ImageFrame;
