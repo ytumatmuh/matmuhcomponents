c010e576ddd023ad2de4c5c92c98c599898f801b
Kuzey Sınay <kuzeysinay@gmail.com>
2024-04-01 17:14:04 +0300

    lecture component service fix

--- src/Segments/LectureSegment/lectureSegment.js
+++ src/Segments/LectureSegment/lectureSegment.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "./LectureComp.css";
+import "./lectureSegment.css";
 import { FiBook } from "react-icons/fi";
 import LectureService from "../../Services/lectureService";
 
 const LectureComp = ({lecture}) => {
   return (
-    <div className="lecture" onKeyUp={lecture.key}>
+    <div className="lecture">
         <div className="lecture-content">
           <h2 className="lecture-title">{lecture.name}</h2>
 
-          {content.map((file) => (
-            <div className="content">
+          {lecture.content.map((file, index) => (
+            <div className="content" key={index}>
               <div className="icon-box">
                 <FiBook />
               </div>
@@ -22,15 +22,16 @@ const LectureComp = ({lecture}) => {
   );
 };
 
-const Lecture = () => {
+const LectureSegment = () => {
   const lectures = LectureService.getLecture();
 
   return(
-    <div className="lecture-container">
-      {lectures.map((lecture)=>(<LectureComp key={lecture.id} lecture={lecture} />))}
+    <div className="lecture-wrapper">
+      <div className="lecture-container">
+        {lectures.map((lecture, index)=>(<LectureComp key={index} lecture={lecture} />))}
+      </div>
     </div>
-
   )
 }
 
-export default Lecture;
+export default LectureSegment;
--- src/Services/lectureservice.js
+++ src/Services/lectureservice.js
@@ -1,53 +1,50 @@
-import LectureComp from "../Components/Lectures/LectureComp";
+const LectureService = {
+  getLecture: () => {
 
-const lectures = [
-  {
-    id: 1,
-    name: "Introduction to Applied Mathematics",
-    content: [
+    return [
       {
         id: 1,
-        desc: "Syllabus of Introduction to Applied Mathematics",
-        contentUrl: "",
+        name: "Introduction to Applied Mathematics",
+        content: [
+          {
+            id: 1,
+            desc: "Syllabus of Introduction to Applied Mathematics",
+            contentUrl: "",
+          },
+          {
+            id: 2,
+            desc: "Notes of Introduction to Applied Mathematics",
+            contentUrl: "",
+          },
+        ],
       },
       {
         id: 2,
-        desc: "Notes of Introduction to Applied Mathematics",
-        contentUrl: "",
+        name: "Physics 2",
+        content: [
+          {
+            id: 1,
+            desc: "Syllabus of Physics 2",
+            contentUrl: "",
+          },
+          {
+            id: 2,
+            desc: "Notes of Physics 2",
+            contentUrl: "",
+          },
+        ],
       },
-    ],
+      // Add more announcements as needed
+    ];
+  
   },
-  {
-    id: 2,
-    name: "Physics 2",
-    content: [
-      {
-        id: 1,
-        desc: "Syllabus of Physics 2",
-        contentUrl: "",
-      },
-      {
-        id: 2,
-        desc: "Notes of Physics 2",
-        contentUrl: "",
-      },
-    ],
-  },
-  // Add more announcements as needed
-];
 
-function Lectures() {
-  return (
-    <div>
-      {lectures.map((lecture) => (
-        <LectureComp
-          key={lecture.id}
-          name={lecture.name}
-          content={lecture.content}
-        />
-      ))}
-    </div>
-  );
+  getLectureById: (id) => {
+    const lectures = LectureService.getLecture();
+    return lectures.find(lecture=>lecture.id ==id)
+  }
+  
 }
 
-export default Lectures;
+
+export default LectureService;
