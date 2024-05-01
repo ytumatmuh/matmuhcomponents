2767eac6e808b429dee64ae2d1f28673d07be70a
Emre Uslu <emreuslu256@gmail.com>
2024-05-01 19:02:29 +0300

    asda

--- src/Components/SingleAnnouncementComp/singleAnnouncement.js
+++ src/Components/SingleAnnouncementComp/singleAnnouncement.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./singleAnnouncement.css";
+import ButtonCustom from "../CustomButton/button";
+import { useTranslation } from 'react-i18next'
+import AnnouncementService from '../../Services/announcementService'
+import { useParams } from 'react-router-dom';
 
 const AnnouncementDetailComp = ({ announcement }) => {
+  const { t } = useTranslation();
   const { imageUrl, publishDate, title, content, links } = announcement;
 
   return (
     <div className="announcement-detail">
-      {imageUrl && (
-        <img src={imageUrl} alt="Announcement" className="announcement-detail-photo" />
+      {announcement.imageUrl && (
+        <img src={announcement.imageUrl} alt="Announcement" className="announcement-detail-photo" />
       )}
       <div className="announcement-detail-content">
-        <div className="announcement-detail-date">{publishDate}</div>
+        <div className="announcement-detail-date">{announcement.publishDate}</div>
         <div className="announcement-detail-header">
           <div className="announcement-detail-text">
-            <h2 className="announcement-detail-title">{title}</h2>
+            <h2 className="announcement-detail-title">{announcement.title}</h2>
           </div>
         </div>
-        <p className="announcement-detail-description">{content}</p>
-        
-        {/* Linkleri göstermek için bir map fonksiyonu kullanın */}
+        <p className="announcement-detail-description">{announcement.content}</p>
         {links && links.length > 0 && (
           <div className="announcement-detail-links">
             <ul>
@@ -31,9 +35,47 @@ const AnnouncementDetailComp = ({ announcement }) => {
             </ul>
           </div>
         )}
+        
       </div>
+      
     </div>
   );
 };
 
-export default AnnouncementDetailComp;
+
+
+const AnnouncementDetail = () => {
+  const { id } = useParams();
+  const [announcements, setAnnouncements] = useState([]);
+  const announcement = announcements.find(announcement => announcement.id === parseInt(id));
+
+  
+
+  useEffect(() => {
+    const fetchAnnouncements = async () => {
+      try {
+        const data = await AnnouncementService();
+        setAnnouncements(data);
+      } catch (error) {
+        console.error('Error fetching announcements:', error);
+      }
+    };
+
+    fetchAnnouncements();
+  }, []);
+
+  // Add a check to ensure announcement is defined before rendering
+  return (
+    <div>
+      {announcement && (
+        <AnnouncementDetailComp
+          key={announcement.id}
+          announcement={announcement}
+        />
+      )}
+    </div>
+  );
+};
+
+
+export default AnnouncementDetail;
