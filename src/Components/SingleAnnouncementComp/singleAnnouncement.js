c9a14d022cd9553f6bf1d487417f9e6c68ce3ddc
Emre Uslu <emreuslu256@gmail.com>
2024-05-01 18:26:50 +0300

    deneme

--- src/Components/SingleAnnouncementComp/singleAnnouncement.js
+++ src/Components/SingleAnnouncementComp/singleAnnouncement.js
@@ -4,44 +4,39 @@ import AnnouncementService from '../../Services/announcementService';
 
 const AnnouncementDetail = () => {
   const { id } = useParams();
-  const [announcements, setAnnouncements] = useState([]);
+  const [announcement, setAnnouncement] = useState(null);
   
   useEffect(() => {
-    const fetchAnnouncements = async () => {
+    const fetchAnnouncement = async () => {
       try {
-        const data = await AnnouncementService();
-        setAnnouncements(data);
+        const data = await AnnouncementService(id); // Call AnnouncementService with id parameter
+        setAnnouncement(data);
       } catch (error) {
-        console.error('Error fetching announcements:', error);
+        console.error('Error fetching announcement:', error);
       }
     };
 
-    fetchAnnouncements();
-  }, []);
+    fetchAnnouncement();
+  }, [id]); // Include id in the dependency array
 
-  // Render null if announcements are still loading
-  if (announcements.length === 0) {
-    return null;
-  }
-
-  // Find the announcement with the provided id
-  const announcement = announcements.find(announcement => announcement.id === parseInt(id));
-
-  // Render null if announcement with the provided id is not found
+  // Render null if announcement is still loading
   if (!announcement) {
     return null;
   }
-    return (
-      <div>
-        <img>{announcement.imageUrl}</img>
-        <p>{announcement.publishDate}</p>
-        <h2>{announcement.title}</h2>
-        <p>{announcement.content}</p>
-        <p>{announcement.content}</p>
-        <a>{announcement.links}</a>
-        
-      </div>
-    );
+
+  return (
+    <div>
+      <img src={announcement.imageUrl} alt={announcement.title} />
+      <p>{announcement.publishDate}</p>
+      <h2>{announcement.title}</h2>
+      <p>{announcement.content}</p>
+      <ul>
+        {announcement.links.map(link => (
+          <li key={link.id}><a href={link.link}>{link.link}</a></li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default AnnouncementDetail;
